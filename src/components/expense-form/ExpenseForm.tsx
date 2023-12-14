4f6bf2048313de7ee78fd676e5364721c56f3d15
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-12-14 13:05:12 +0300

    Create expense form using react hook form

--- src/components/expense-form/ExpenseForm.tsx
+++ src/components/expense-form/ExpenseForm.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { Button, Form } from 'react-bootstrap'
 
 const ExpenseForm = () => {
   return (
-    <div>ExpenseForm</div>
-  )
-}
+    <Form>
+      <Form.Group className='mb-3' controlId='expense_type'>
+      <Form.Label>Expense Type</Form.Label>
+    <Form.Select aria-label="Expense Type">
+      <option value="">Select Expense Type</option>
+      <option value="cash">Cash</option>
+      <option value="card">Card</option>
+    </Form.Select>
+    </Form.Group>
+    <Form.Group className="mb-3" controlId="expense_date">
+        <Form.Label>Expense Date</Form.Label>
+        <Form.Control type="date" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="expense_amount">
+        <Form.Label>Expense Amount (In USD)</Form.Label>
+        <Form.Control type="text" placeholder='Enter amount' />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="description">
+        <Form.Label>Description</Form.Label>
+        <Form.Control as="textarea" rows={3} placeholder='Enter description' />
+      </Form.Group>
+      <Form.Group>
+      <Button variant="success">Add Expense</Button>
+      </Form.Group>
+    </Form>
+  );
+};
 
 export default ExpenseForm
