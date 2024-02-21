f7ceb2e4c99f1f8708e206be099a1d6c1c2f5aa5
Emmanuel Balikuddembe <balixemma@gmail.com>
2024-02-21 12:18:19 +0300

    pass isLoggedIn prop

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,21 @@
-import React, { FC } from 'react'
-import Sidebar from './Sidebar/Sidebar'
+import React, { FC } from "react";
+import Sidebar from "./Sidebar/Sidebar";
 interface LayoutProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
+  isLoggedIn: boolean;
 }
 
-const Layout: FC<LayoutProps> = ({children}) => {
+const Layout: FC<LayoutProps> = ({ children, isLoggedIn }) => {
   return (
-    <div className='main'>
-    <div className='left'>
-        <Sidebar />
-    </div>
-    <div className='right'>{children}</div>
+    <div className="main">
+      {isLoggedIn && (
+        <div className="left">
+          <Sidebar />
+        </div>
+      )}
+      <div className="right">{children}</div>
     </div>
   );
 };
 
-export default Layout
+export default Layout;
